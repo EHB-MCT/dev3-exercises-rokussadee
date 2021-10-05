5bcb7c6dd57ff7cae4a5961175e123c82de17fbb
rokussadee <rokus.sadee@student.ehb.be>
2021-10-05 17:36:51 +0200

    rock, paper, scissors (basic)

---
+++ session1/src/main/kotlin/rps.kt
@@ -0,0 +1,44 @@
+fun main() {
+    val options = arrayOf("rock", "paper", "scissors")
+
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("The computer chose $computerChoice")
+    println("You chose $userChoice")
+
+    println(determineWinner(userChoice, computerChoice))
+}
+
+fun getUserChoice(options: Array<String>): String {
+
+    val question = "rock, paper or scissors?"
+    var userChoice = ""
+
+    do {
+        println(question)
+        userChoice = readLine().toString()
+    } while (!options.contains(userChoice))
+
+    return userChoice
+}
+
+fun determineWinner(userChoice: String, computerChoice: String): String {
+    if (userChoice == computerChoice) {
+        return "It's a tie. Try again!"
+    } else if((userChoice == "paper" && computerChoice == "rock") || (userChoice == "scissors" && computerChoice == "paper") || (userChoice == "rock" && computerChoice == "scissors")) {
+        return "You Won!"
+    } else {
+        return "You Lost!"
+    }
+}
+
+fun playGame(userInput: String, randomOption: String) {
+    if ((userInput == "paper" && randomOption == "rock") || (userInput == "scissors" && randomOption == "paper") || (userInput == "rock" && randomOption == "scissors")) {
+        println("You Won!")
+    } else if(userInput == randomOption) {
+        println("It's a tie. Try again!")
+    } else {
+        println("You lost. Try again")
+    }
+}
